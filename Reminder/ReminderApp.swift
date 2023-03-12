c7ebb07096c82d02c8ba64c4b9aca79b60379c6b
KanataWithCJ <1312518723@qq.com>
2023-03-12 11:32:58 +0800

    Test commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by 从径源 on 2023/3/12.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
